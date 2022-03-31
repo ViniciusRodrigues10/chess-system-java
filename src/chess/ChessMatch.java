283f5ca45813cb531a6c1514213d8619f405d4bd
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2022-03-31 13:48:11 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -84,7 +84,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position terget) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(terget);
 		board.placePiece(p, terget);
 		
@@ -97,7 +98,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		
 		if (capturedPiece != null) {
